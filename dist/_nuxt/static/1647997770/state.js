d255491862d47c00e6be986ed352eddc1e5149f5
samgordon444 <admin@samgordon.design>
2022-03-22 20:09:41 -0500

    generated dist

---
+++ dist/_nuxt/static/1647997770/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1647997770",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6296432f"}}}}(null,"\u002F"));
